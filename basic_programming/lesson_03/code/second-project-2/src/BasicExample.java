de2e56c9b0d030ac16b561a2faaf8b2213d12d87
d.tkachenko <d.tkachenko@argustelecom.ru>
2024-03-13 13:57:27 +0300

    lesson 3

---
+++ basic_programming/lesson_03/code/second-project-2/src/BasicExample.java
@@ -0,0 +1,36 @@
+public class BasicExample {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.println("a + b");
+        System.out.println(50 * b);
+        System.out.println("50 * b");
+
+        int c = 10;
+
+        System.out.println(c * b);
+        System.out.println(c / b);
+
+        int d = c * 15;
+
+        int e = a * b * c; //чему равняется e
+        int f = a / a + c + c;
+        int g = b * b * b * b * b;
+
+        System.out.println(g);
+
+    }
+
+}
+
+
+
+
+//переменная
+//x = 5
+//daniil = 100
+//y = 45
+//z = 78.5
---
+++ basic_programming/lesson_03/code/second-project-2/src/Overload.java
@@ -0,0 +1,8 @@
+public class Overload {
+
+    public static void main(String[] args) {
+        int a = 2_000_000_000;
+        int b = a + a;
+        System.out.println(b);
+    }
+}
