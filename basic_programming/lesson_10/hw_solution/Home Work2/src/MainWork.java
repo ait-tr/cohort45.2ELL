6f7e431f8453cd3e00372a1af7903a17f37ab3a6
Eugen Murzov <nellyeu@hotmail.de>
2024-04-08 18:49:51 +0000

    (HW)-user: 'Eugen_Murzov-nellyeu@hotmail.de', commit time: '2024-04-08 18:49:51'

---
+++ basic_programming/lesson_10/hw_solution/Home Work2/src/MainWork.java
@@ -0,0 +1,10 @@
+public class MainWork {
+    public static void main(String[] args) {
+        //1. Сделать метод, который выводит на экран сообщение "Я имею писать методы".
+
+         String string = "Я умею писать методы";
+
+         System.out.println("Я умею писать методы");
+    }
+
+}
---
+++ basic_programming/lesson_10/hw_solution/Home Work2/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo java!");
+        System.out.println("My name is Eugen");
+        System.out.println("I am 33 years old");
+        System.out.println("I come from Russia");
+
+
+    }
+}
