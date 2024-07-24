2eddc89ec2a3fda8001d25192ced2794b331b748
Aleksandr Gerstenberger <alex_7377@hotmail.com>
2024-07-24 07:28:53 +0000

    (HW)-user: 'Aleksandr_Gerstenberger-alex_7377@hotmail.com', commit time: '2024-07-24 07:28:53'

---
+++ basic_programming/lesson_39/hw_solution/src/PackageMain.java
@@ -0,0 +1,57 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Класс PackageMain содержит метод main, который является точкой входа в приложение.
+ * <p>
+ * В данном классе инициализируется список посылок с различными параметрами,
+ * такими как город отправления, город назначения, вес и дата отправления.
+ * Также осуществляется поиск самой новой посылки, отправляющейся в Берлин.
+ */
+public class PackageMain {
+
+    public static void main(String[] args) {
+
+        List<Package> packages = new ArrayList<>();
+        packages.add(new Package("New York", "Berlin", 2.5,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("America/New_York"))));
+        packages.add(new Package("Tokyo", "Berlin", 3.0,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("Asia/Tokyo"))));
+        packages.add(new Package("Moscow", "Berlin", 1.2,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("Europe/Moscow"))));
+        packages.add(new Package("Sydney", "Berlin", 1.5,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("Australia/Sydney"))));
+        packages.add(new Package("London", "Berlin", 4.0,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("Europe/London"))));
+        packages.add(new Package("Paris", "Berlin", 4.0,
+                ZonedDateTime.of(LocalDateTime.of(2024, 7, 20, 10, 0),
+                        ZoneId.of("Europe/Paris"))));
+
+        System.out.println("\nВсе посылки из списка:");
+        packages.forEach(System.out::println);
+
+        Package latestPackageToBerlin = packages.stream()
+                .filter(pkg -> pkg.getCityOfReceipt().equalsIgnoreCase("Berlin"))
+                .max(Comparator.comparing(Package::getCreationDateTimeInUTC))
+                .orElse(null);
+
+        try {
+            if (latestPackageToBerlin == null) {
+                throw new Exception("Нет посылок, отправляющихся в Берлин.");
+            }
+            System.out.println("\nСамая новая посылка в Берлин:\n" + latestPackageToBerlin);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
---
+++ basic_programming/lesson_39/hw_solution/src/ProgramminglanguageMain.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Класс ProgramminglanguageMain - класс,
+ * <p>
+ * который инициализирует список языков программирования,
+ * <p>
+ * выводит их на экран и определяет самый старый язык по дате его первой версии.
+ */
+public class ProgramminglanguageMain {
+
+    public static void main(String[] args) {
+
+        List<Programminglanguage> languages = new ArrayList<>();
+        languages.add(new Programminglanguage
+                ("Java", "James Gosling", LocalDate.of(1995, 5, 23)));
+        languages.add(new Programminglanguage
+                ("C", "Dennis Ritchie", LocalDate.of(1972, 5, 1)));
+        languages.add(new Programminglanguage
+                ("Python", "Guido van Rossum", LocalDate.of(1991, 2, 20)));
+        languages.add(new Programminglanguage
+                ("C++", "Bjarne Stroustrup", LocalDate.of(1985, 10, 1)));
+        languages.add(new Programminglanguage
+                ("JavaScript", "Brendan Eich", LocalDate.of(1995, 12, 4)));
+        languages.add(new Programminglanguage
+                ("Fortran", "John Backus", LocalDate.of(1957, 1, 1)));
+
+        System.out.println("\nВсе языки из списка:");
+        languages.forEach(System.out::println);
+
+        Programminglanguage oldestLanguage = languages.stream()
+                .min(Comparator.comparing(Programminglanguage::getDateOfFirstVersion))
+                .orElse(null);
+
+        try {
+            if (oldestLanguage == null) {
+                throw new Exception("Список языков программирования пуст.");
+            }
+            System.out.println("\nСамый старый язык программирования:\n" + oldestLanguage);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
